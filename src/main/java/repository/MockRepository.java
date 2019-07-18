553ee010fe263b20fce78111a54e2287147d8f11
jinu <jjw8967@ajou.ac.kr>
2019-07-19 00:50:17 +0900

    Implement addMovie(), removeMovie(), reserveMovie(), returnChangeMoney() on MockService

--- src/main/java/repository/MockRepository.java
+++ src/main/java/repository/MockRepository.java
@@ -8,10 +8,16 @@ public interface MockRepository {
 
     Movie findByName(String name);
 
-    List<Movie> findByTime(int time);
+    List<Movie> findByTime(int movieTime);
 
     List<Movie> findByPrice(int price);
 
     List<Movie> findAll();
+
+    Movie updateSeatOfMovie(Movie movie, int seat);
+
+    void addMovie(Movie movie);
+
+    void removeMovie(Movie movie);
 
 }
